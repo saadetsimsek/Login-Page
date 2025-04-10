7fdc24a18097e13032c709f13b3513551e107e89
saadetsimsek <saadetsimsek13@gmail.com>
2025-04-10 21:47:49 +0300

    Initial Commit

---
+++ LoginPageTests/LoginPageTests.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageTests.swift
+//  LoginPageTests
+//
+//  Created by Saadet Şimşek on 10/04/2025.
+//
+
+import Testing
+@testable import LoginPage
+
+struct LoginPageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
